e24f52412d36ee78660335a0dc027bf8e6e4551e
thisisommore <ommore501@gmail.com>
2021-07-21 03:18:27 +0000

    Don't update kernel

--- .gitpod/install.sh
+++ .gitpod/install.sh
@@ -1,5 +1,5 @@
 sudo apt update -y
-sudo apt install qemu qemu-system-x86 linux-image-$(uname -r) libguestfs-tools sshpass -y
+sudo apt install qemu qemu-system-x86 libguestfs-tools sshpass -y
 sudo curl -o /usr/bin/kubectl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo chmod +x /usr/bin/kubectl
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
